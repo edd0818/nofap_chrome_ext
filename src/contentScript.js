00bd91f311bb832a94b774318d0e1001e1cd8df9
Eddie.Hsieh <eddie.hsieh@hy-tech.com.tw>
2020-06-10 23:26:57 +0800

    send fapping notification from content script

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,14 +1,19 @@
-import {HTMLUtil} from './mist/utils';
+import {HTMLUtil, GASUtils} from './mist/utils';
 import {default as keywords} from './adult_keywords.json'
-
+import {state, actions, getters, mutations} from './store/store';
 
 let pageInfo = HTMLUtil.getMetaInfos(document, 'title', 'description', 'keyword');
 pageInfo.push(document.title);
 
 const keywordsRegex = new RegExp(keywords.join('|'));
-
 let isAdultContent = keywordsRegex.test(pageInfo.join(' '));
+
 if (isAdultContent) {
+    // send fapping notification
+    actions.fetchSettings().then(settings => {
+        GASUtils.sendFappingNotification(settings.notification_recipient);
+    })
+    // redirect to no-fap home page.
     const homePageUrl = chrome.runtime.getURL("home/home.html");
     document.getElementsByTagName('body')[0].innerHTML += 
     "<div style='opacity:0.95; filter: alpha(opacity=30); background-color:#000;" +
