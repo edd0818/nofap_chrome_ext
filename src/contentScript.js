8b6e4bc652f62e577dc3b7b6740407a73da9619d
Eddie.Hsieh <eddie.hsieh@hy-tech.com.tw>
2020-06-10 23:27:42 +0800

    send fapping notification from content script-1

--- src/contentScript.js
+++ src/contentScript.js
@@ -20,5 +20,3 @@ if (isAdultContent) {
     "width:100%; height:100%; z-index:10; top:0; left:0; position:fixed; '></div>";
     chrome.runtime.sendMessage({redirect: homePageUrl});
 }
-
-console.log('idAdultContent: ', isAdultContent);
